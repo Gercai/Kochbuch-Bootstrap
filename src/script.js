efe737dd50bd670c1d18da017309018038e205c1
Gerald <53179363+Gercai@users.noreply.github.com>
2023-02-08 16:45:17 +0000

    added Card links

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
 
+// var $myGroup = $('#myGroup');
+// $myGroup.on('show.bs.collapse','.collapse', function() {
+//     $myGroup.find('.collapse.in').collapse('hide');
+// });
+
+// $('[data-toggle="collapse"]').click(function() {
+//     $('.collapse.in').collapse('hide')
+//   });
