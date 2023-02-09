deeaa8a6be78955b9147bba199099c495fbefd30
Gerald <53179363+Gercai@users.noreply.github.com>
2023-02-09 14:10:58 +0000

    new function added

--- src/script.js
+++ src/script.js
@@ -7,9 +7,26 @@
 //     $('.collapse.in').collapse('hide')
 //   });
 
-function shareRecipe(){
+function shareRecipe(e){
+    
     const url = window.location.href
+
     window.alert(`the page at localhost: ${url} says:\n\n\n my alert message`);
+    
+};
+
+function shareRecipe2(e){
+
+//    console.log(e.currentTarget.parentElement.parentElement.parentElement.parentElement.id)
+    const food = e.currentTarget.parentElement.parentElement.parentElement.parentElement.id;
+    window.alert(`the page at localhost: ${food} says:\n\n\n I am ${food}`);
+    
+};
+
+let shareButton =  document.getElementsByClassName("share");
+
+for(let i = 0; i < shareButton.length; i++){    
+shareButton[i].addEventListener('click', shareRecipe2, false);
 };
 
 
